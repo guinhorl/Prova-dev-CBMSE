e41c97bfd8427bd3278906d47441d9aff8b53739
Wagner Ramos Limas <50498155+guinhorl@users.noreply.github.com>
2020-10-19 13:25:24 -0300

    Ajuste
    Ajustes

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/codeIgniter/prova-dev-CBMSE';
+$config['base_url'] = 'http://localhost/codeIgniter/Prova-dev-CBMSE';
 
 /*
 |--------------------------------------------------------------------------
